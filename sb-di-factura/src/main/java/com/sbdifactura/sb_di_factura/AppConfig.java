b8b18d254e077d4119b57a1a27d6994560ded456
ElShogun <jesusdavidd3@gmail.com>
2024-05-28 06:49:36 -0700

    final

--- sb-di-factura/src/main/java/com/sbdifactura/sb_di_factura/AppConfig.java
+++ sb-di-factura/src/main/java/com/sbdifactura/sb_di_factura/AppConfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 
 import com.sbdifactura.sb_di_factura.models.Item;
@@ -23,6 +24,21 @@ public class AppConfig {
         Product product2 = new Product("Portatil asus", 33333);
 
         List<Item> items = Arrays.asList(new Item(product, 2), new Item(product2,4));
+
+        return items;
+    }
+
+    @Bean
+    @Primary
+    List<Item> invoOfiiceItems(){
+        Product product1 = new Product("Monitor 1", 300);
+        Product product2 = new Product("Monitor 2", 400);
+        Product product3 = new Product("Monitor 3", 500);
+        Product product4 = new Product("Monitor 4", 600);
+        Product product5 = new Product("Portatil asus", 33333);
+
+        List<Item> items = Arrays.asList(new Item(product1, 2), new Item(product2,3),
+        new Item(product3,4), new Item(product4, 5), new Item(product5,6));
 
         return items;
     }
--- sb-di-factura/src/main/java/com/sbdifactura/sb_di_factura/models/Client.java
+++ sb-di-factura/src/main/java/com/sbdifactura/sb_di_factura/models/Client.java
@@ -3,6 +3,9 @@ package com.sbdifactura.sb_di_factura.models;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 @Component
 public class Client {
 
@@ -12,6 +15,19 @@ public class Client {
     @Value("${client.lastname}")
     private String lastname;
 
+    public Client() {
+        
+    }
+
+    @PostConstruct
+    public void init(){
+        //Trabajar con los atributos del objeto una vez inicializado
+    }
+
+    @PreDestroy
+    public void preDestroy(){
+        //hacer algo antes de destruir el objeto
+    }
     public String getName() {
         return name;
     }
